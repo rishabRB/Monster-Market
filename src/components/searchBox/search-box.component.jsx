bec2de52cbd9ab0ab9b8c5cc28cc80d0b31725e0
Rishab Bibhuty <rishab@debian.debian>
2021-01-28 22:39:34 +0530

    Updates

--- src/components/searchBox/search-box.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from'react';
-
-import '../searchBox/search-box.style.component.css';
-
-
-export const SearchBox=({placeholder,handler})=>(
-    <input className='search' type='search' placeholder={placeholder} onChange={handler}/>
-);
